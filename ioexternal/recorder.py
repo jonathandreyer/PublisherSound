8041c39c15ce1481bb23c5dcba4efe94e53e6551
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2017-07-17 16:32:15 +0200

    Add wrapper on event method

--- ioexternal/recorder.py
+++ ioexternal/recorder.py
@@ -5,17 +5,36 @@ import RPi.GPIO as GPIO
 
 class Recorder:
     GPIO_BTN = 27
+    BOUNCETIME_MS = 250
 
     def __init__(self, func):
         self.logger = logging.getLogger('app.ioexternal.Recorder')
         self.logger.debug('init')
-        self._func = func
+
+        self._fcs = list()
+
+        if not isinstance(func, list):
+            if callable(func):
+                self._fcs.append(func)
+            else:
+                raise Exception('Not a callable function!')
+        else:
+            for f in func:
+                if callable(f):
+                    self._fcs.append(f)
+                else:
+                    raise Exception('Not a callable function!')
 
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
 
         GPIO.setup(self.GPIO_BTN, GPIO.IN)
-        GPIO.add_event_detect(self.GPIO_BTN, GPIO.FALLING, callback=self._func, bouncetime=25)
+        GPIO.add_event_detect(self.GPIO_BTN, GPIO.FALLING, callback=self._event, bouncetime=self.BOUNCETIME_MS)
+
+    def _event(self, channel):
+        self.logger.debug('event on channel %s' % channel)
+        #for fc in self._fcs:
+        self._fcs[0]()
 
     def __del__(self):
         self.logger.debug('del.')
