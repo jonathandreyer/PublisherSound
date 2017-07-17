8bf05faed3b30a217c2d2feba293bd4944553695
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2017-07-17 15:28:07 +0200

    Create recorder class..
    .. with input event from GPIO

---
+++ ioexternal/recorder.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import logging
+import RPi.GPIO as GPIO
+
+
+class Recorder:
+    GPIO_BTN = 27
+
+    def __init__(self, func):
+        self.logger = logging.getLogger('app.ioexternal.Recorder')
+        self.logger.debug('init')
+        self._func = func
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.add_event_detect(self.GPIO_BTN, GPIO.RISING, callback=self._func, bouncetime=25)
+
+    def __del__(self):
+        self.logger.debug('del.')
+        GPIO.remove_event_detect(self.GPIO_BTN)
+        GPIO.cleanup(self.GPIO_BTN)
+
+
+if __name__ == "__main__":
+    import time
+
+    logger = logging.getLogger('app')
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+
+    # Console output
+    consolelog = logging.StreamHandler()
+    consolelog.setFormatter(formatter)
+    logger.addHandler(consolelog)
+    logger.setLevel(logging.DEBUG)
+
+    def event():
+        print('hello')
+
+    rec = Recorder(event)
+
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print('interrupted!')
